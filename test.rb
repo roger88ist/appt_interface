16ba714e2bc0d7005e8e959860724772808b7ae9
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-02 12:28:37 -0400

    added the code to submit a put request to the api using httparty

--- test.rb
+++ test.rb
@@ -2,19 +2,26 @@ require 'httparty'
 
 
 url = "http://localhost:3000/api/appointments"
+url5 = "http://localhost:3000/api/appointments/5"
 
 
 # This is the code for a GET request
-response = HTTParty.get(url)
-puts response["status"]
+# response = HTTParty.get(url)
+# puts response["status"]
 
 # This is the code for a POST request
-response = HTTParty.post(url,
-  { 
-    :body => {"appointment" => { "first_name" => "Alex", "last_name" => "Vera", "comments" => "Coming into Scale" }}.to_json,
-    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-  })
+# response = HTTParty.post(url,
+#   { 
+#     :body => {"appointment" => { "first_name" => "Alex", "last_name" => "Vera", "comments" => "Coming into Scale" }}.to_json,
+#     :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+#   })
+# puts response.body
+
+# This is the code for a PUT request
+response = HTTParty.put(url5,
+	{
+		:body => {"appointment" => { "comments" => "Changed this comment using httparty put request"}}.to_json,
+		:headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+	})
 puts response.body
-
-
 
