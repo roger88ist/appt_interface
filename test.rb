80a2b8d65814e66fa9fce9bd0bab5143069a23fe
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-02 12:16:23 -0400

    Found out how to send a POST request using HTTParty and puts out to the terminal screen the response given when that type of request is made.

--- test.rb
+++ test.rb
@@ -1,16 +1,20 @@
 require 'httparty'
 
+
 url = "http://localhost:3000/api/appointments"
 
-# response = HTTParty.get(url)
 
-# puts response["status"]
+# This is the code for a GET request
+response = HTTParty.get(url)
+puts response["status"]
 
-HTTParty.post(url,
+# This is the code for a POST request
+response = HTTParty.post(url,
   { 
-    :body => {"appointment" => { "first_name" => "Juha", "last_name" => "Mikkola", "comments" => "This was done with httparty" }}.to_json,
+    :body => {"appointment" => { "first_name" => "Alex", "last_name" => "Vera", "comments" => "Coming into Scale" }}.to_json,
     :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
   })
+puts response.body
 
-# response = HTTParty.get(url)
-# puts response
+
+
